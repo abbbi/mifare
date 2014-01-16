d4b10da3da04df36f3a89a2822c88798335029d0
Michael Ablassmeier <ma@sep.de>
2014-01-16 10:16:21 +0100

    meh

--- myfare.py
+++ myfare.py
@@ -4,10 +4,11 @@ prot = {
     'enquiry_module': '\x03\x12\x00\x15',
     'enquiry_module_return': '\x02\x12\x14',
     'active_buzzer': '\x02\x13\x15',
-    'enquiry_cards': '\x03\x02\x00\x05',
-    'enquiry_cards_return': '\x03\x02\x01\x06', # 03020106
+    'enquiry_card': '\x03\x02\x00\x05',
+    'enquiry_cards_return': '\x03\x02\x01\x06', # should return hex 03020106 according to protocol
     'enquiry_all_cards': '\x03\x02\x01\x05',
     'anticollision' : '\x02\x03\x05',
+    'select_card' : '\x02\x04\x06',
 }
 
 ser = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
@@ -22,7 +23,18 @@ f = ser.read(10)
 
 if f == prot['active_buzzer']:
     print "buzzer is active"
+    
+# enquiry should return hex 03020106 according to protocol
+# but reader doesnt, need a known working rfid card.
+ser.write(prot['enquiry_card'])
+print ser.read(10).encode('hex')
 
-ser.write(prot['enquiry_all_cards'])
-data = ser.read(10)
-print data.encode('hex')
+# should return something starting with 0603
+ser.write(prot['anticollision'])
+print ser.read(10).encode('hex')
+
+# should return 020406
+ser.write(prot['select_card'])
+print ser.read(10).encode('hex')
+
+
